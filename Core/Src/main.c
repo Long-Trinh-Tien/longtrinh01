58f0adf03e5e71711663b871e99be971a1c841fe
Long-Trinh-Tien <113412392+Long-Trinh-Tien@users.noreply.github.com>
2022-11-09 00:53:43 +0700

    ex2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -52,8 +52,6 @@ static void MX_TIM2_Init(void);
   */
 int main(void)
 {
-
-
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
@@ -85,12 +83,18 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-		HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, 1);
-		HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, 0);
-		HAL_Delay(2000);
-		HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, 1);
-		HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, 0);
-		HAL_Delay(2000);
+		HAL_GPIO_WritePin(LED_GREEN_GPIO_Port,LED_GREEN_Pin , 1);//tat green
+		HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, 1);//tat yellow
+		HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, 0);//bat red
+		HAL_Delay(5000);//5s for red
+
+		HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, 1);//tat red
+		HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, 0);//bat yellow
+		HAL_Delay(2000);//2s for yellow
+
+		HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, 1);//tat yellow
+		HAL_GPIO_WritePin(LED_GREEN_GPIO_Port,LED_GREEN_Pin , 0);//bat green
+		HAL_Delay(3000);//3s for green
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -191,10 +195,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YELLOW_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YELLOW_Pin;
+  /*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin LED_GREEN_Pin */
+  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
